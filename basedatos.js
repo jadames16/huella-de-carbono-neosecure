7b29f96d8b35a173c67e2c8b02cad30d106d5c42
jadames16 <147514386+jadames16@users.noreply.github.com>
2025-06-17 10:51:53 -0500

    Add files via upload

---
+++ basedatos.js
@@ -0,0 +1,13 @@
+// Configuración de Firebase (usa tus propios valores)
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
