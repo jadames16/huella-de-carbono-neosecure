aeb5697cb5d2c174912e11bb96367f15484d218e
jadames16 <147514386+jadames16@users.noreply.github.com>
2025-06-17 11:17:56 -0500

    Update basedatos.js

--- basedatos.js
+++ basedatos.js
@@ -1,11 +1,12 @@
 // Configuración de Firebase (usa tus propios valores)
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+    apiKey: "AIzaSyDHgKJXxQf6B3aQrHOcnFqdM3UkXiuF4RA",
+    authDomain: "huella-de-carbono-neo.firebaseapp.com",
+    projectId: "huella-de-carbono-neo",
+    storageBucket: "huella-de-carbono-neo.firebasestorage.app",
+    messagingSenderId: "145000733830",
+    appId: "1:145000733830:web:ef8695d898f97024c44507",
+    measurementId: "G-ZMSHCN03M0"
 };
 
 // Inicializa Firebase
